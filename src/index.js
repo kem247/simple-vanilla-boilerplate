fabeaaa3af4c73cc3ebe53f090eaf8b18efe034a
Kemi Thomas <kemothom71@gmail.com>
2020-03-02 13:06:49 -0500

    added chatFunction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import input from "./chatFunction";
 import axios from "axios";
 
+const chatBox = document.getElementsByClassName("popup__chatbox");
+const chatInput = document.getElementsByClassName("popup__input");
+
+chatInput[0].addEventListener("keydown", async e => {
+  if (13 == e.keycode) {
+    chatBox.style.display = await input(e.target.value);
+  }
+});
+
 // console.log(sum(1,2));
 
 // const main = async () => {
